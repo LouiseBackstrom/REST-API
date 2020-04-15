ab11ab580f3b02237482e387a939d65d21df83fa
Louise Bäckström <backstromlouise@gmail.com>
2020-04-15 13:05:30 +0200

    added get and disney characters

--- index.js
+++ index.js
@@ -2,6 +2,60 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//disney characters
+const dCharacters = [
+    {   
+        id: 0,
+        name: "Elsa",
+        fairyTale: "Frost",
+        bestFriend: "Anna",
+        img: ""
+    },
+    {   
+        id: 1,
+        name: "Robin Hood",
+        fairyTale: "Robin Hood",
+        bestFriend: "Piglet",
+        img: ""
+    },
+    {   
+        id: 2,
+        name: "Winnie the Pooh",
+        fairyTale: "Robin Hood",
+        bestFriend: "Little John",
+        img: ""
+    },
+    {   
+        id: 3,
+        name: "Princess Jasmine",
+        fairyTale: "Aladdin",
+        bestFriend: "Aladdin",
+        img: ""
+    },
+    {   
+        id: 4,
+        name: "Simba",
+        fairyTale: "Lion King",
+        bestFriend: "Nala",
+        img: ""
+    }
+]
+
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get('/dCharacters', (req, res) => {
+    res.json(dCharacters)
+})
+
+app.get('/dCharacters/:dCharacterId', (req, res) => {
+    const character = dCharacters.find((dCharacter) => 
+    (dCharacter.id.toString() === req.params.dCharacterId)) 
+            if(!character) {
+                res.status(404).send()
+            } else {
+                res.send(character)
+            }
+    })
 
 app.listen(port, () => console.log(`listening at http://localhost:${port}`))
